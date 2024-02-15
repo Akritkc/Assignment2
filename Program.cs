97105a95d8fbc9b12fd54a75ba38c2d8f7176469
Akritkc <kcakrit@gmail.com>
2024-02-14 20:16:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Interval
+{
+    static void Main()
+    {
+        // ask the user to enter a real number
+        Console.Write("Enter a real number: ");
+        double x = double.Parse(Console.ReadLine());
+
+        // Checking if the entered number belongs to the specified interval I
+        bool belongsToI = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+        // Displaying the result based on the test
+        if (belongsToI)
+        {
+            Console.WriteLine($"x belongs to I");
+        }
+        else
+        {
+            Console.WriteLine($"x does not belong to I");
+        }
+    }
+}
